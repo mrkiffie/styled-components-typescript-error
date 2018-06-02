d7f4f0b83a3e1a07c4e35fe59d275d7d3eb9940f
mrkiffie <mrkiffie@gmail.com>
2018-06-02 23:12:31 +0200

    Add minimal change to showcase error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
+import styled from "styled-components";
 import "./App.css";
 
 import logo from "./logo.svg";
+
+const H1 = styled.h1`
+  color: red;
+`;
 
 class App extends React.Component {
   public render() {
@@ -9,7 +14,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <H1 className="App-title">Welcome to React</H1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
